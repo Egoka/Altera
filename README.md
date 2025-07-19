5cdee3223b11f3a7f18878a1c9ef35b4c01c90cc
Egoka <eg.bonda2014@gmail.com>
2025-07-19 18:23:10 +0300

    build: the project is configured

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Altera
-Altera is an open cultural publishing platform where anyone can write and share articles on art, music, photography, sports, and travel — with an elegant editor based on MDC (Markdown + Components).
+[![Banner.png](.github/assets/banner.png)]()
+
+<h1 align="center">
+Altera
+</h1>
+<p align="center">
+A different word. A different culture.
+<p>
+
+**Altera** is a digital magazine and creative platform where culture, art, sports, music, photography, travel, and human thought come together.  
+Here, words sound clear, form is precise, and topics know no boundaries.
+
+We believe that culture is not an archive, but a living fabric.  
+It manifests in gestures, in rhythm, in routes, in frames, and in language.
+
+Altera invites you not just to read, but to participate.  
+Every user can register and publish their own articles — from essays to visual chronicles, from music reviews to reflections on sport as a discipline of the spirit.
+
+**Writing is simple:**  
+Materials are created from ready-made semantic blocks in the MDC format — a new editorial methodology that combines the expressiveness of Markdown with the flexibility of components.  
+*Visually precise. Structurally clean. Human.*
+
+---
+
+**Altera is a space for respectful words.**  
+No clichés. No emptiness. No algorithmic noise.  
+Only texts backed by thought.  
+Only a voice backed by a person.
---
+++ eslint.config.mjs
@@ -0,0 +1,80 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tsParser from "@typescript-eslint/parser"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import vuePlugin from "eslint-plugin-vue"
+import vueParser from "vue-eslint-parser"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// импорт кастомного правила
+// const noConsoleLog = await import(path.resolve(__dirname, "eslint-rules/no-console-log.js")).then(m => m.default ?? m);
+
+export default [
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.nuxt/**",
+      "**/coverage/**",
+    ]
+  },
+
+  // === Для TypeScript и JavaScript файлов ===
+  {
+    files: ["**/*.ts", "**/*.js"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      local: {
+        rules: {}
+      }
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-dynamic-delete": "off",
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"]
+    }
+  },
+
+  // === Для Vue файлов (.vue) ===
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue"]
+      }
+    },
+    plugins: {
+      vue: vuePlugin,
+      "@typescript-eslint": typescriptEslint,
+      local: {
+        rules: {}
+      }
+    },
+    rules: {
+
+      // Vue
+      "vue/no-unused-vars": "error",
+      "vue/html-self-closing": "off",
+      "vue/no-v-html": "off",
+
+      // TypeScript внутри <script lang="ts">
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/consistent-type-definitions": ["error", "interface"]
+    }
+  }
+]
